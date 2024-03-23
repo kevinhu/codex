0f16833812076a2b258abf7033e6223a9d746ba0
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2024-03-23 15:25:38 -0700

    entity page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,59 +1,52 @@
-import { useState } from "react";
-import { Toaster } from "sonner";
-import MistralClient from "@mistralai/mistralai";
-import { useLocalStorage } from "usehooks-ts";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [apiKey, setApiKey] = useLocalStorage<string>("api_key", "");
-  const [response, setResponse] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
+  const [searchResults, setSearchResults] = useState<
+    {
+      id: string;
+      name: string;
+    }[]
+  >([]);
 
-  const listModels = async () => {
-    const client = new MistralClient(apiKey);
-
-    try {
-      const streamResponse = await client.chatStream({
-        model: "mistral-large-latest",
-        messages: [
-          { role: "user", content: "What is the best French cheese?" },
-        ],
-      });
-
-      setResponse("");
-
-      for await (const chatResponse of streamResponse) {
-        setResponse(
-          (response) => response + chatResponse.choices[0].delta.content
-        );
-      }
-
-      console.log(response);
-    } catch (error) {
-      console.error(error);
+  useEffect(() => {
+    if (query.length === 0) {
+      setSearchResults([]);
+      return;
     }
-  };
+
+    setSearchResults([
+      {
+        id: "1",
+        name: "Result 1",
+      },
+      {
+        id: "2",
+        name: "Result 2",
+      },
+    ]);
+  }, [query]);
 
   return (
     <div className="flex flex-col items-center">
-      <Toaster />
       <div className="flex flex-col w-full max-w-screen-md space-y-2 py-12">
-        <label htmlFor="api_key" className="text-2xl">
-          API Key
-        </label>
+        <h1>Codex</h1>
         <input
-          id="api_key"
+          id="search"
           type="text"
-          value={apiKey}
-          onChange={(e) => setApiKey(e.target.value)}
+          value={query}
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
           className="border border-gray-300 rounded px-2 py-1"
         />
-        <button
-          onClick={listModels}
-          className="bg-blue-500 text-white rounded px-3 py-1"
-        >
-          Test endpoint
-        </button>
-        <div className="border border-gray-300 rounded px-2 py-1 bg-gray-50 whitespace-pre-wrap">
-          {response || "What is the best French cheese?"}
+        <div className="border border-gray-300 rounded px-2 py-1 bg-gray-50 flex flex-col">
+          {searchResults.map((result) => (
+            <Link key={result.id} to={result.id}>
+              {result.name}
+            </Link>
+          ))}
+          {searchResults.length === 0 && "No results"}
         </div>
       </div>
     </div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Entity } from "./Entity.tsx";
+import { loader as entityLoader } from "./router.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/:id",
+    element: <Entity />,
+    loader: entityLoader,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
